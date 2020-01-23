fbd65491810dab565767768f11e7616c25f3f9b9
shahadlaml <50049930+shahadlaml@users.noreply.github.com>
2020-01-23 16:35:03 +0300

    added the 404 page and make the landing page logo scrool effect

--- js/nav.js
+++ js/nav.js
@@ -6,9 +6,35 @@ $(document).ready(function(){
        $(this).toggleClass('btn-open').toggleClass('btn-close');
        event.preventDefault();
     });
-});
+
 $('.overlay').on('click', function(){
     $(".overlay").fadeToggle(200);
     $(".button a").toggleClass('btn-open').toggleClass('btn-close');
     open = false;
+});
+
+
+//the landing page scrool
+
+
+        // $(window).bind('scroll', function() {
+        //     $("#navigation").removeClass("landing-page-head");
+        // });
+
+        $("#landing-logo").attr("src","../img/logo/white.svg");
+$(window).bind('scroll', function() {
+    var scrollPos = $(window).scrollTop();
+    if (scrollPos <= 0) {
+        $("#navigation").addClass("landing-page-head");
+        $("#landing-logo").attr("src","../img/logo/white.svg");
+    } else {
+        $("#navigation").removeClass("landing-page-head");
+        $("#landing-logo").attr("src","../img/logo/fullcolor.svg");
+    }
+});
+
+
+
+
+
 });
